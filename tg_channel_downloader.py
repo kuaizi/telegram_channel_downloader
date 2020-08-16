66bbe4644de3e217a2935b67fcf3879cb3510544
snow922841 <61658739+snow922841@users.noreply.github.com>
2020-08-16 14:18:22 +0800

    修复bug
    修复bug

--- tg_channel_downloader.py
+++ tg_channel_downloader.py
@@ -1,20 +1,17 @@
 # -*- coding:utf-8 -*-
-import time
-
+import datetime
 from pyrogram import Client
-import os
+import os, re
 from tqdm import tqdm
 ################################### config ###############################################
-
-save_path = 'E:\\tmp'          # 文件保存路径 Linux 系统 一般路径格式为 '/tmp/download'
-channel_name = '@example'      # 频道名称或 群组数字ID 如果为数字 则不需要单引号 如 123456
-offset_id = 0                  # 消息偏移量， int类型，从指定的消息ID开始遍历。
-reverse = False                # 从新的消息往旧的消息遍历，如果改为True则从旧消息往后遍历
-
+black_list = ['7z', 'rar', 'zip']               # 过滤黑名单，列表中的文件格式不下载。
+save_path = f'E:\\tmp\\{channel_name}'          # 文件保存路径 Linux 系统 一般路径格式为 '/tmp/download'
+chat_id = '@example'                            # 频道名称或群组名称
+offset_id = 0                                   # 消息偏移量， int类型，从指定的消息ID开始遍历。
+reverse = False                                 # 从新的消息往旧的消息遍历，如果改为True则从旧消息往后遍历
+limit = 0                                       # 限制返回消息数量
 ##########################################################################################
 
-app = Client("my_account")
-app.start()
 
 
 class TqdmUpTo(tqdm):
@@ -26,28 +23,63 @@ class TqdmUpTo(tqdm):
     def update_to(self, current, total):
         self.update(current - self.last_block)
         self.last_block = current
+        
+        
+def validateTitle(title):
+    r_str = r"[\/\\\:\*\?\"\<\>\|\n]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(r_str, "_", title)  # 替换为下划线
+    return new_title
 
+def main():
+    for message in app.iter_history(chat_id=chat_id, offset_id=offset_id, reverse=reverse, limit=limit):
+        if message.media:
+            # print(message)
+            # 标题
+            caption = message.caption.replace(':', '：') if message.caption != None else ""
 
-for message in app.iter_history(chat_id=channel_name, offset_id=offset_id, reverse = reverse):
-    try:
-        if message.document.file_name in os.listdir(save_path):
-            pass
-        else:
-            if message.document.file_name.endswith('.7z'): #过滤指定类型文件。
-                continue
-            #elif message.document.file_name.endswith('.zip'):
-            #    continue
-            #elif message.document.file_name.endswith('.rar'):
-            #    continue
+            # 相册ID
+            group_id = message.media_group_id if message.media_group_id != None else ""
+
+            # 文件夹名
+            dir_name = datetime.datetime.fromtimestamp(message.date).strftime("%Y年%m月")
+            file_save_path = os.path.join(save_path, dir_name)
+
+            # 如果文件夹不存在则创建文件夹
+            if not os.path.exists(file_save_path):
+                os.makedirs(file_save_path)
+
+            # 如果是图片
+            if message.photo != None:
+                file_name = f'{message.message_id}-{group_id}-{caption}.jpg'
+                total = message.photo.file_size
+            # 如果是视频
+            elif message.video != None:
+                file_name = f'{message.message_id}-{group_id}-{caption}.mp4'
+                total = message.video.file_size
+            # 如果是文件
+            elif message.document != None:
+                file_name = f'{caption}-' + message.document.file_name
+                total = message.document.file_size
             else:
-                tqdm = TqdmUpTo(total=message.document.file_size,
-                                desc=f'{message.message_id} - {message.document.file_name}',
-                                unit='B', unit_scale=True)
-                message.download(file_name=os.path.join(save_path, message.document.file_name),
-                                 progress=tqdm.my_update,
-                                 block=True)
-                tqdm.close()
-                time.sleep(0.01)
+                # print(message.message_id, '\n')
+                continue
 
-    except Exception as e:
-        continue
+            file_name = validateTitle(file_name)  # 去掉文件名称中的特殊字符
+
+            if file_name in os.listdir(file_save_path):
+                continue
+            elif file_name.split('.')[-1] in black_list:
+                continue
+            else:
+                t = TqdmUpTo(total=total, desc=file_name, unit='B', unit_scale=True)
+                message.download(file_name=os.path.join(file_save_path, file_name), progress=t.my_update)
+                t.close()
+
+if __name__ == '__main__':
+    app = Client("my_account")
+    app.start()
+    # chat_id = "@MRHXPJ"
+    # chat_id = "@mengxinsetu" -5506
+    # chat_id =   "@ONEPIECETHKyoto" # - 69985
+    # chat_id = '@okjfresh' # 6850
+    main()
